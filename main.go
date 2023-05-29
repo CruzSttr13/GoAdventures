6daf005e34b118a9e4228f2396404c3a2732c414
CruzSttr13 <jcruz_rmz98@outlook.com>
2023-05-29 16:25:53 -0600

    the differents methods to run the program

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import "fmt"
 
 //The main function is the entry point of our application.
 func main() {
-	fmt.Println("Hola mundo desde Go")
+	fmt.Println("It's better to use the run command to skip a step.")
 
 }
+
+//commands for console
+//"go run main.go" to run the program in the console
+//"go build main.go" to build the program in the console
+//"./main" to run the program in the console after building it
