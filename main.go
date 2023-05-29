4b04dbb9a42a49d8be03c8195effdf729ed7431c
CruzSttr13 <jcruz_rmz98@outlook.com>
2023-05-29 16:14:12 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+// we define the package name as main, which means that this is the entry point of our application.
+package main
+
+//importing the fmt package, which contains functions for printing text to the console.
+import "fmt"
+
+//The main function is the entry point of our application.
+func main() {
+	fmt.Println("Hola mundo desde Go")
+
+}
